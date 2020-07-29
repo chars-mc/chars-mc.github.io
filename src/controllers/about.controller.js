6071085827624ec882673e3ce2859631a66542dd
chars <chars_2000xsp@hotmail.com>
2020-07-28 21:46:33 -0500

    about view

--- src/controllers/about.controller.js
+++ src/controllers/about.controller.js
@@ -1,8 +1,32 @@
 import viewHtml from '../views/about.hbs';
+import technologies from '../js/technologies.json';
 
 const view = document.createElement('div');
 view.innerHTML = viewHtml();
 
 export default () => {
    return view;
+}
+
+(function init() {
+   view.querySelector('#skills').innerHTML = getList(technologies.frontend);
+   view.querySelector('#skills').innerHTML += getList(technologies.backend);
+   view.querySelector('#skills').innerHTML += getList(technologies.tools);
+
+   view.querySelector('#other').innerHTML = getList(technologies.other);
+   view.querySelector('#learning').innerHTML = getList(technologies.learning);
+}())
+
+function getList(techs) {
+   let list = '';
+   techs.forEach((tech) => {
+      list += `
+         <div class="technology">
+            <i class="${tech.icon}"></i>
+            <p>${tech.name}</p>
+         </div>
+      `;
+   });
+
+   return list;
 }
